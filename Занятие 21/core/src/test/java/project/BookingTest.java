1bb35290f89953ecd914ac343595ea3e5701d87d
RomanLosev <47692245+RomanLosev@users.noreply.github.com>
2019-03-27 23:54:22 +0300

    Add lesson 21 (#15)

---
+++ Занятие 21/core/src/test/java/project/BookingTest.java
@@ -0,0 +1,46 @@
+package project;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BookingTest {
+
+	private WebDriver driver;
+	private BookingPage bookingPage;
+
+	@Before
+	public void before() {
+		driver = new ChromeDriver();
+		bookingPage = new BookingPage(driver);
+		driver.get("http://www.booking.com");
+
+	}
+
+	@Test
+	public void test() throws Throwable {
+
+		bookingPage.enterCityAndData("Москва");
+
+		Assert.assertTrue(bookingPage.islinkHotelPresent());
+
+		bookingPage.clickMaxRatingkHotel();
+
+		bookingPage.clickHotel();
+
+		Assert.assertTrue(bookingPage.ratingButton());
+
+	}
+
+	@After
+	public void tearDown() {
+
+		driver.quit();
+	}
+
+}
