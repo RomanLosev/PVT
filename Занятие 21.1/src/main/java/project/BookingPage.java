fcbb8b14c6a045f83cb3c82e31d2b622a7d79c7c
RomanLosev <47692245+RomanLosev@users.noreply.github.com>
2019-03-28 22:57:09 +0300

    Add lesson 21.1 (#16)

---
+++ Занятие 21.1/src/main/java/project/BookingPage.java
@@ -0,0 +1,87 @@
+package project;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BookingPage {
+
+	@FindBy(xpath = ".//input[@type=\"search\"]")
+	WebElement cityField;
+
+	@FindBy(xpath = "(.//span[contains(text(), \"30\")])[1]")
+	WebElement dataField;
+
+	@FindBy(xpath = "(.//img[@class=\"hotel_image\"])[1]")
+	WebElement linkHotel;
+
+	@FindBy(xpath = ".//button[@id=\"sortbar_dropdown_button\"]")
+	WebElement allRating;
+
+	@FindBy(xpath = ".//li[@class=\" sort_category   sort_bayesian_review_score \"]")
+	WebElement maxRating;
+
+	@FindBy(xpath = ".//div[@id=\"sr_notice_minimum_number_of_reviews\"]")
+	WebElement linkButton;
+	
+	@FindBy(xpath = "(.//a[@class=\"hotel_name_link url\"])[1]")
+	WebElement firstHotel;
+
+	@FindBy(xpath = ".//div[@class=\"bui-review-score__badge\"]")
+	WebElement ratingButton;
+
+	private WebDriver driver;
+	private WebDriverWait wait;
+
+	public BookingPage(WebDriver driver) {
+		this.driver = driver;
+		wait = new WebDriverWait(driver, 10);
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public void enterCityAndData(String city) {
+
+		cityField.sendKeys(city);
+		cityField.submit();
+		dataField.click();
+		dataField.submit();
+
+	}
+
+	public boolean islinkHotelPresent() {
+
+		return linkHotel.isDisplayed();
+	}
+
+	public void clickMaxRatingkHotel() {
+
+		allRating.click();
+		maxRating.click();
+
+	}
+
+	public void clickHotel() {
+
+		wait.until(ExpectedConditions.visibilityOfAllElements(linkButton));
+		firstHotel.click();
+
+	}
+
+	public boolean ratingButton() {
+
+		String raiting = ratingButton.getText();
+		String raiting1 = raiting.replaceAll(",", ".");
+		double raiting2 = Double.parseDouble(raiting1);
+
+		return raiting2 >= 9;
+
+	}
+
+}
