e641140c08484916af7cd1eae68cfe77aa3de21e
RomanLosev <47692245+RomanLosev@users.noreply.github.com>
2019-07-10 22:06:40 +0300

    Add RomanLosev (#27)

---
+++ RomanLosev/Task2.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter some name: ");
+		String name = scanner.nextLine();
+
+		if (name.equals("Vyacheslav")) {
+			System.out.println("Hello, Vyacheslav");
+		} else {
+			System.out.println("No such name");
+		}
+	}
+
+}
